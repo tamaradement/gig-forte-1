ab4feddc4039e77574f79af53ccbb57fffaa3413
Tamara deMent <tamara.dement@gmail.com>
2022-08-15 17:28:14 -0400

    Added S3 settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "pages.apps.PagesConfig",
     "tunes.apps.TunesConfig",
     "gigs.apps.GigsConfig",
+    # S3 
+    "storages",
 ]
 
 MIDDLEWARE = [
@@ -114,49 +116,46 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
+# Amazon S3 Settings (This is the new stuff)
+AWS_ACCESS_KEY_ID = env.str('AWS_ACCESS_KEY_ID')
 
-# Amazon S3 Settings
-# AWS_ACCESS_KEY_ID = env.str('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env.str('AWS_SECRET_ACCESS_KEY')
 
-# AWS_SECRET_ACCESS_KEY = env.str('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env.str('AWS_STORAGE_BUCKET_NAME')
 
-# AWS_STORAGE_BUCKET_NAME = env.str('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_DEFAULT_ACL = 'public-read'
 
-# AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400'
+}
 
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400'
-# }
+AWS_LOCATION = 'static'
 
-# AWS_LOCATION = 'static'
+AWS_QUERYSTRING_AUTH = False
 
-# AWS_QUERYSTRING_AUTH = False
+AWS_HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+}
 
-# AWS_HEADERS = {
-#     'Access-Control-Allow-Origin': '*',
-# }
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
-# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-
-# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 
-# static 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
-STATIC_ROOT = str(BASE_DIR.joinpath("staticfiles"))
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-MEDIA_URL = "tunes/"
-STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
-STATIC_ROOT = str(BASE_DIR.joinpath("staticfiles"))
+# Old stuff 
+# STATIC_URL = "/static/"
+# STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
+# STATIC_ROOT = str(BASE_DIR.joinpath("staticfiles"))
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# MEDIA_URL = "tunes/"
+# STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
+# STATIC_ROOT = str(BASE_DIR.joinpath("staticfiles"))
 
 
 
