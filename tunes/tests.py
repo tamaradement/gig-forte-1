6274ba48abefca69ac2a3092c2b80e313efcb21d
Tamara deMent <tamara.dement@gmail.com>
2022-09-26 11:34:21 -0400

    Added two tests for tune_list view

--- tunes/tests.py
+++ tunes/tests.py
@@ -1,12 +1,17 @@
 from accounts.models import CustomUser
 from django.test import TestCase
 from django.urls import reverse
+from django.test.client import RequestFactory
+from django.contrib.auth.models import AnonymousUser
+from .views import TuneListView
 from .models import Tune
 
 class TuneTests(TestCase): 
     @classmethod
 
     def setUpTestData(cls):
+        cls.factory = RequestFactory()
+
         cls.user = CustomUser.objects.create_user(
             username="testuser", 
             email="test@email.com", 
@@ -33,3 +38,17 @@ class TuneTests(TestCase):
         self.assertEqual(f"{self.tune.notes}", "Some notes")
         self.assertEqual(f"{self.tune.genre}", "Classical")
         self.assertEqual(f"{self.tune.performer}", "testuser")
+
+    def test_tune_list_view(self):
+        request = self.factory.get('/tunes/')
+        request.user = AnonymousUser()
+        response = TuneListView.as_view()(request)
+        self.assertEqual(response.status_code,302)
+    
+    def test_tune_list_view_with_valid_user(self):
+        request = self.factory.get('/tunes/')
+        request.user = self.user
+        response = TuneListView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.template_name[0], 'tunes/tune_list.html')
+
