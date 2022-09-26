e412399a52ade8c8bcffe5b7f85b241d5c793f5b
Tamara deMent <tamara.dement@gmail.com>
2022-09-26 09:37:39 -0400

    Added two tests for Tune functionality

--- tunes/tests.py
+++ tunes/tests.py
@@ -1,3 +1,35 @@
+from accounts.models import CustomUser
 from django.test import TestCase
+from django.urls import reverse
+from .models import Tune
 
-# Create your tests here.
+class TuneTests(TestCase): 
+    @classmethod
+
+    def setUpTestData(cls):
+        cls.user = CustomUser.objects.create_user(
+            username="testuser", 
+            email="test@email.com", 
+            password="secret", 
+            instrument="violin",
+        )
+        
+        cls.tune = Tune.objects.create(
+              title="A good title",
+              composer="Bach",
+              key="C Major",
+              notes="Some notes",
+              genre="Classical",
+              performer=cls.user,
+        )
+
+    def test_get_absolute_url(self): 
+        self.assertEqual(self.tune.get_absolute_url(), "/tunes/1/")
+
+    def test_tune_content(self): 
+        self.assertEqual(f"{self.tune.title}", "A good title") 
+        self.assertEqual(f"{self.tune.composer}", "Bach") 
+        self.assertEqual(f"{self.tune.key}", "C Major")
+        self.assertEqual(f"{self.tune.notes}", "Some notes")
+        self.assertEqual(f"{self.tune.genre}", "Classical")
+        self.assertEqual(f"{self.tune.performer}", "testuser")
