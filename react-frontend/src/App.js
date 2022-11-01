12d6f45b7d46f02fd59f3c21c9830a63bf8136fe
Tamara deMent <tamara.dement@gmail.com>
2022-10-31 20:42:35 -0400

    Another react-related production experiment

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,69 +1,25 @@
-import './App.css'
-import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-// class Tune extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h2><a href={`http://127.0.0.1:8000/tunes/${this.props.tune.id}/`}>{this.props.tune.title}</a></h2>
-//         <p>{this.props.tune.composer} | {this.props.tune.genre}</p>
-//       </div>
-//     );
-//   }
-
-// }
-
-class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //       isLoading: true,
-  //       tunes: [],
-  //   };
-
-  //   this.handleSuccess = this.handleSuccess.bind(this);
-  //   this.handleError = this.handleError.bind(this); 
-  // }
-
-  // componentDidMount() {
-  //   fetch("http://127.0.0.1:8000/tunes/tunes_api/")
-  //     .then((response => response.json()))
-  //     .then(this.handleSuccess, this.handleError);
-  // }
-
-  // handleSuccess(responseData) {
-  //   console.log(responseData);
-
-  //   this.setState({
-  //     isLoading: false,
-  //     tunes: responseData,
-  //   });
-  // }
-
-  // handleError(error) {
-  //   console.log(error);
-  // }
-
-  render() {
-    // let tuneList = [];
-    // for (let i = 0; i < this.state.tunes.length; i++) {
-    //   const tune = this.state.tunes[i];
-    //   tuneList.push(<Tune key={i} tune={tune} />);
-    // }
-
-    return (
-      <div>
-        <h1>Tune List</h1>
-        {/* <br></br>
-        {tuneList} */}
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-
 
 export default App;
