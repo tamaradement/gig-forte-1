7649c9166a58640027028e2b1ef7365e3c30fdc5
Tamara deMent <tamara.dement@gmail.com>
2022-11-02 08:42:28 -0400

    Fetch and display data in main App component

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,9 +2,49 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tunes: [],
+      header: 'Tune List'
+    }
+
+    this.handleSuccess = this.handleSuccess.bind(this);
+    this.handleError = this.handleError.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:8000/tunes/tunes_api/")
+      .then((response => response.json()))
+      .then(this.handleSuccess, this.handleError);
+  }
+
+  handleSuccess(response) {
+    this.setState({
+      tunes: response
+    })
+  }
+
+  handleError(error) {
+    
+    this.setState({
+      header: 'Something went wrong...'
+    })
+    console.log(error);
+  }
+
   render() {
+    const tunes = [];
+    for (let i = 0; i < this.state.tunes.length; i++) {
+      const tune = this.state.tunes[i];
+      tunes.push(<p>{tune.title}</p>);
+    }
+
     return (
-      <h1>Tune List</h1>
+      <div>
+        <h1>{this.state.header}</h1>
+        {tunes}
+      </div>   
     );
   }
 }
