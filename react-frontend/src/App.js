f03edf39b20216bf87ba9d83b9926c0747468c55
Tamara deMent <tamara.dement@gmail.com>
2022-11-02 08:16:14 -0400

    Changed main app component to a class component

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <h1>Tune List</h1>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <h1>Tune List</h1>
+    );
+  }
 }
-
 export default App;
